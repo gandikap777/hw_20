b0d199b2e19e9a71a5b6d98687abfe5d001357a4
kolominov.a <kolominov.a@redmond.company>
2021-06-04 12:08:39 +0300

    регистрация

---
+++ homework_20/Models/UserRegistrtion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homework_20.Service
+{
+    public class UserRegistration
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required, MaxLength(20)]
+        [Display(Name = "Логин")]
+        public string LoginProp { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare(nameof(Password))]
+        [Display(Name = "Подтверждение пароля")]
+        public string ConfirmPassword { get; set; }
+    }
+}
